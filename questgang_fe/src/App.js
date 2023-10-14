fbcaa77b44ea29103d226d44948acd5117bfd2b6
Mahonri <ray22004@byui.edu>
2023-10-13 19:46:22 -0600

    Push to branch so I can work on my pc:D

--- questgang_fe/src/App.js
+++ questgang_fe/src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
+import background from './qg_forest.jpg';
 import './App.css';
+import React, { Component } from 'react';
 
+
+function backgroundStyle() {
+  return {
+    backgroundImage: `${background}`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100vw',
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    zIndex: '-1',
+  }
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Quest Gang, see us in the forest you better run.
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"

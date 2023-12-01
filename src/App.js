5258f60813639501995c6a0fd13077823d1e07d8
gavhart <har19076@byui.edu>
2023-11-30 17:23:10 -0700

    finished the store page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import './App.css';
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Homepage';
-import Store from './pages/Store';
-import Cave from './pages/Cave';
-import Forest from './pages/Forest';
+import "./App.css";
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Homepage";
+import Store from "./pages/Store";
+import Cave from "./pages/Cave";
+import Forest from "./pages/Forest";
+
+
 
 function App() {
-  return <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/Store' element={<Store />} />
-    <Route path='/Cave' element={<Cave />} />
-    <Route path='/Forest' element={<Forest />} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Store" element={<Store />} />
+      <Route path="/Cave" element={<Cave />} />
+      <Route path="/Forest" element={<Forest />} />
+    </Routes>
+  );
 }
 
 export default App;
 
-{/* <img src={logo} className="App-logo" alt="logo" /> */}
-{/* <a
+{
+  /* <img src={logo} className="App-logo" alt="logo" /> */
+}
+{
+  /* <a
 className="App-link"
 href="https://reactjs.org"
 target="_blank"
 rel="noopener noreferrer"
 >
 Learn React
-</a> */}
-
+</a> */
+}
 
 // return (
 //   <>
 //     <div className="App">
 //       <header className="App-header">
 //         <LoginForm />
-        
+
 //         <FetchWizClass />
 
 //         <Icon name = "Rogue" sprite = {Sprite}/>
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,11 +1,48 @@
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './store.css';
 
 function Store() {
-    return (
-        <>
-            <h1>Store</h1>
-        </>
-    )
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:80/store");
+        setItems(response.data.slice(0, 3)); // Get only the first three items
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePurchase = async (itemId) => {
+    try {
+      // Replace with your actual purchase API endpoint
+      await axios.post(`http://localhost:80/store/purchase/${itemId}`);
+      alert("Purchase successful!");
+    } catch (error) {
+      console.error("Error processing purchase:", error);
+      alert("Error processing purchase.");
+    }
+  };
+
+  return (
+    <div className="store-container">
+      <div className="items-container">
+        {items.map((item) => (
+          <div key={item.id} className="item">
+            <h2>{item.name}</h2>
+            <p>{item.description}</p> {/* Display item description */}
+            <p>Price: {item.sellPriceInGold} Gold Pieces</p> {/* Display item price */}
+            <button onClick={() => handlePurchase(item.id)}>Buy Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Store;

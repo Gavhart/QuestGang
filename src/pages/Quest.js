81397278eeb847e1a0fee31eb651c7c6865ba8dd
gavhart <har19076@byui.edu>
2023-12-05 20:14:54 -0700

    rough

--- src/pages/Quest.js
+++ src/pages/Quest.js
@@ -1,47 +1,61 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Quest() {
-    const [quests, setQuests] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [quests, setQuests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchQuests = async () => {
-            try {
-                const response = await axios.get('http://localhost:80/quests/request?userId=1701375677110566&userLevel=1&numQuests=3');
-                console.log('Response:', response); // Debugging log
-                setQuests(response.data);
-                setLoading(false);
-            } catch (err) {
-                console.error('Error fetching quests:', err); // Debugging log
-                setError(err);
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchQuests = async () => {
+      try {
+        const response = await axios.put(
+          "http://localhost:80/quests/request?userId=17014836893499660&userLevel=1&numQuests=3"
+        );
+        console.log("Response:", response);
+        setQuests(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching quests:", err);
+        setError(err);
+        setLoading(false);
+      }
+    };
 
-        fetchQuests();
-    }, []);
+    fetchQuests();
+  }, []);
 
-    if (loading) return <div>Loading quests...</div>;
-    if (error) return <div>Error loading quests: {error.message}</div>;
+  if (loading) return <div>Loading quests...</div>;
+  if (error) return <div>Error loading quests: {error.message}</div>;
 
-    return (
-        <div>
-            <h2>Available Quests</h2>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                {quests.map((quest, index) => (
-                    <div key={index} style={{ border: '1px solid black', margin: '10px', padding: '20px' }}>
-                        <Link to={`/quest/${quest.id}`}> {/* Make sure your routing is set up correctly */}
-                            <h3>{quest.title}</h3>
-                            <p>{quest.description}</p>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Available Quests</h2>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {quests.map((quest, index) => (
+          <div
+            key={index}
+            style={{
+              border: "1px solid black",
+              margin: "10px",
+              padding: "20px",
+            }}
+          >
+            <Link to={`/quest/${quest.id}`}>
+              {" "}
+              {/* Make sure your routing is set up correctly */}
+              <h3>{quest.title}</h3>
+              <p>{quest.description}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+      ''
+    </div>
+  );
 }
 
 export default Quest;

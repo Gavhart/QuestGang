3094ccdb4053eea362b72aafe7293cac0929fa22
Mahonri <ray22004@byui.edu>
2023-12-07 13:00:04 -0700

    Attack commit

--- src/pages/Forest.js
+++ src/pages/Forest.js
@@ -6,15 +6,24 @@ function Forest() {
     userId: "",
     choice: ""
   })
-  const [userStatus, setUserStatus] = useState("NOT_IN_COMBAT");
+  const [userStatus, setUserStatus] = useState("IN_COMBAT");
+  const [userId, setUserId] = useState();
+  const [battleMessage, setBattleMessage] = useState()
+  const [choices, setChoices] = useState()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:80/character/status?username=testUser");
         const jsonStatus = response.data.status;
+        const getUserId = response.data.userId
+        setChoices(jsonStatus.choices)
         setUserStatus(jsonStatus.userStatus);
-        console.log("User Status:", jsonStatus);
+        setUserId(getUserId)
+        console.log(response.data)
+        // console.log("User Status:", jsonStatus);
+        console.log("userId: ", userId);
+        console.log(choices)
       } catch (error) {
         console.error("Error fetching user status:", error);
       }
@@ -23,6 +32,36 @@ function Forest() {
     fetchData();
   }, []); // Empty dependency array ensures this effect runs once after the initial render
 
+const GoLeft = async () =>{
+  const gl_form = {
+    userId: "1701375677110566",
+    choice: "2"
+  }
+
+  await axios.put("http://localhost:80/quest/choice", gl_form)
+  // console.log(GLMessage)
+}
+
+const GoRight = async () =>{
+}
+
+const RunAway = async() =>{
+  const form ={
+    userId: userId,
+    action: "run"
+  }
+  axios.put("http://localhost:80/quest/action", form)
+}
+const Attack = async () =>{
+  const form = {
+    userId: userId,
+    action: "attack"
+  }
+  const attackResponse = await axios.put("http://localhost:80/quest/action", form)
+  setBattleMessage(attackResponse.data.message)
+  // console.log(attackResponse.data.message)
+}
+
   console.log("User Status: ", userStatus);
 
   return (
@@ -30,14 +69,15 @@ function Forest() {
       <h1>Forest</h1>
       {userStatus === "IN_QUEST" &&(
         <div className = "in_quest">
-            <button>Go Left</button> 
-            <button>Go Right</button>
+            <button className="go_left" onClick={GoLeft}>Go Left</button> 
+            <button className="go_right" onClick={GoRight}>Go Right</button>
         </div> 
        )}
        {userStatus === "IN_COMBAT" &&(
         <div className="in_combat">
-           <button>Run</button>
-           <button>Attack</button>
+          <h1>{battleMessage}</h1>
+           <button className="run_button" onClick={RunAway}>Run</button>
+           <button className="attack_button" onClick={Attack}>Attack</button>
         </div>
        )}
     </div>

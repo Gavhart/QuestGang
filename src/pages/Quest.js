08696b6cad3bed4fbebcd52dca0c2fcc0285e843
gavhart <har19076@byui.edu>
2023-12-07 14:32:24 -0700

    finished the quests

--- src/pages/Quest.js
+++ src/pages/Quest.js
@@ -3,46 +3,45 @@ import axios from "axios";
 import './quest.css';
 
 function Quest() {
-        const [quests, setQuests] = useState([]);
-        const [error, setError] = useState(null);
-        const [userId, setUserId] = useState("1701375677110566");
-    
-        useEffect(() => {
-            const fetchQuests = async () => {
-                try {
-                    const response = await axios.put("http://localhost:80/quests/request?userId="+userId);
-                    if (response?.data) {
-                        // Randomly select three quests
-                        console.log (response.data);
-                        setQuests(response.data.quests);
-                    } else {
-                        setQuests([]);
-                    }
-                } catch (err) {
-                    console.error("Error fetching quests:", err);
-                    setError(err);
+    const [quests, setQuests] = useState([]);
+    const [error, setError] = useState(null);
+    const [userId, setUserId] = useState("1701375677110566");
+
+    useEffect(() => {
+        const fetchQuests = async () => {
+            try {
+                const response = await axios.put("http://localhost:80/quests/request?userId="+userId);
+                if (response?.data) {
+                    setQuests(response.data.quests);
+                } else {
+                    setQuests([]);
                 }
-            };
-    
-            fetchQuests();
-        }, []);
-    
-        if (error) return <div>Error loading quests: {error.message}</div>;
-    
-        return (
-            <div className="quests-container">
-                <h2>Available Quests</h2>
-                <div className="quests-list">
-                    {quests.map((quest, index) => (
-                        <div key={index} className="quest">
-                            <h3>{quest.name}</h3>
-                            <p>{quest.difficulty}</p>
-                        </div>
-                    ))}
-                </div>
+            } catch (err) {
+                console.error("Error fetching quests:", err);
+                setError(err);
+            }
+        };
+    }, []);
+
+    if (error) return <div>Error loading quests: {error.message}</div>;
+
+    return (
+        <div className="quests-container">
+            <h2>Available Quests</h2>
+            <div className="quests-list">
+                {quests.map((quest, index) => (
+                    <div key={index} className="quest">
+                        <h3>{quest.name}</h3>
+                        <p>{quest.difficulty}</p>
+                    </div>
+                ))}
             </div>
-        );
-    }
-    
-    export default Quest;
-    
+            {/* Static message under quest boxes */}
+            <div className="quest-message">
+                {quests.length > 0 ? "Select a quest to continue" : "No quests available at the moment"}
+            </div>
+        </div>
+    );
+                
+                }
+export default Quest;

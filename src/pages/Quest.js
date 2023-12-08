c9a856516964184bc8078ae48859efc849111ccc
gavhart <har19076@byui.edu>
2023-12-08 16:10:24 -0700

    Quest page works!!

--- src/pages/Quest.js
+++ src/pages/Quest.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import './quest.css';
 
 function Quest() {
     const [quests, setQuests] = useState([]);
     const [error, setError] = useState(null);
-    const [userId, setUserId] = useState("1701375677110566");
+    const [userId, setUserId] = useState("1701375677110566"); // username: testUser
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchQuests = async () => {
@@ -21,7 +24,28 @@ function Quest() {
                 setError(err);
             }
         };
+
+        fetchQuests();
     }, []);
+
+    const selectQuest = (quest) => async () => {
+        try {
+            console.log(quest)
+            const response = await axios.put("http://localhost:80/quests/accept?userId="+userId+"&questId="+quest.questId);
+            if (response?.status === 200) {
+                if (quest.locations[0] === "Cave")
+                    navigate("/Cave")
+                else if (quest.locations[0] === "Forest")
+                    navigate("/Forest")
+                else if (quest.locations[0] === "Mountain")
+                    navigate("/Mountain")
+            }
+        } catch (e) {
+            console.error("Error accepting quest", e)
+            // setError(e)
+        }
+        // localStorage.setItem('quest', JSON.stringify(quest));
+    };
 
     if (error) return <div>Error loading quests: {error.message}</div>;
 
@@ -30,7 +54,7 @@ function Quest() {
             <h2>Available Quests</h2>
             <div className="quests-list">
                 {quests.map((quest, index) => (
-                    <div key={index} className="quest">
+                    <div key={index} className="quest" onClick={selectQuest(quest)}>
                         <h3>{quest.name}</h3>
                         <p>{quest.difficulty}</p>
                     </div>
